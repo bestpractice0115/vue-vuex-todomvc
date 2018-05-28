ab75afe4ddc536744ef76c78a3a1d9fb63af99eb
bestpractice0115 <bestpractice0115@gmail.com>
2018-05-28 17:36:44 +0800

    Add store

--- app.js
+++ app.js
@@ -8,6 +8,79 @@
       .substr(2, 10)
   }
 
+  const store = new Vuex.Store({
+    state: {
+      loading: true,
+      todos: [],
+      newTodo: ''
+    },
+    getters: {
+      newTodo: state => state.newTodo,
+      todos: state => state.todos
+    },
+    mutations: {
+      SET_LOADING (state, flag) {
+        state.loading = flag
+      },
+      SET_TODOS (state, todos) {
+        state.todos = todos
+      },
+      SET_NEW_TODO (state, todo) {
+        state.newTodo = todo
+      },
+      ADD_TODO (state, todoObject) {
+        console.log('add todo', todoObject)
+        state.todos.push(todoObject)
+      },
+      REMOVE_TODO (state, todo) {
+        var todos = state.todos
+        todos.splice(todos.indexOf(todo), 1)
+      },
+      CLEAR_NEW_TODO (state) {
+        state.newTodo = ''
+        console.log('clearing new todo')
+      }
+    },
+    actions: {
+      loadTodos ({ commit }) {
+        commit('SET_LOADING', true)
+        axios
+          .get('/todos')
+          .then(r => r.data)
+          .then(todos => {
+            commit('SET_TODOS', todos)
+            commit('SET_LOADING', false)
+          })
+      },
+      setNewTodo ({ commit }, todo) {
+        commit('SET_NEW_TODO', todo)
+      },
+      addTodo ({ commit, state }) {
+        if (!state.newTodo) {
+          // do not add empty todos
+          return
+        }
+        const todo = {
+          title: state.newTodo,
+          completed: false,
+          id: randomId()
+        }
+        axios.post('/todos', todo).then(_ => {
+          commit('ADD_TODO', todo)
+        })
+      },
+      removeTodo ({ commit }, todo) {
+        axios.delete(`/todos/${todo.id}`).then(_ => {
+          console.log('removed todo', todo.id, 'from the server')
+          commit('REMOVE_TODO', todo)
+        })
+      },
+      clearNewTodo ({ commit }) {
+        commit('CLEAR_NEW_TODO')
+      }
+    }
+  })
+
   // app Vue instance
   const app = new Vue({
     store,
